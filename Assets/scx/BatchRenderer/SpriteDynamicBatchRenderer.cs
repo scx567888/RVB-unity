65dde43d3437c35a6f62cbaf59033276b1f29efa
scx567888 <scx567888@outlook.com>
2025-11-04 16:05:48 +0800

    dev

--- Assets/scx/BatchRenderer/SpriteDynamicBatchRenderer.cs
+++ Assets/scx/BatchRenderer/SpriteDynamicBatchRenderer.cs
@@ -5,7 +5,7 @@ using UnityEngine.U2D;
 
 public class SpriteDynamicBatchRenderer : BaseDynamicBatchRenderer<SpriteRenderUnit> {
     
-    private static readonly Material BASE_MATERIAL = Resources.Load<Material>("scx/Unlit-AlphaTest");
+    private static readonly Material BASE_MATERIAL = Resources.Load<Material>("scx/Unlit-Alpha");
 
     private SpriteAtlas _rawSpriteAtlas;
     private Dictionary<string, Vector2[]> _uvs;
