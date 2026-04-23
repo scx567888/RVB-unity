35793c45196600467425cf13e2aa1ec34ed4bf15
scx567888 <scx567888@outlook.com>
2026-04-23 13:37:42 +0800

    dev

---
+++ Assets/scx/SpriteRenderer/ScxSprite.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// SCX 图
+/// </summary>
+public class ScxSprite { 
+    
+    /// 图块名称
+    private string name;
+    
+    /// 该图块在图集中的矩形区域
+    private RectInt atlasRect;
+    
+    /// 原图尺寸 (裁边前)
+    private Vector2Int sourceSize;
+
+    /// 归一化锚点, 通常 0~1
+    private Vector2 pivot;
+    
+}
