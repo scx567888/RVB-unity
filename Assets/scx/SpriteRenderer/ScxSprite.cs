971575fd2f0de6df0ddf9172908f390b674a8984
scx567888 <scx567888@outlook.com>
2026-04-23 13:55:08 +0800

    dev

--- Assets/scx/SpriteRenderer/ScxSprite.cs
+++ Assets/scx/SpriteRenderer/ScxSprite.cs
@@ -3,21 +3,29 @@
 /// <summary>
 /// SCX 图块 (不支持旋转)
 /// </summary>
-public interface ScxSprite { 
-    
+public sealed class ScxSprite {
+
     /// 图块名称
-    string name();
-    
+    public readonly string name;
+
     /// 该图块在图集中的矩形区域
-    RectInt atlasRect();
-    
+    public readonly RectInt atlasRect;
+
     /// 该图块在原图中的矩形区域
-    RectInt sourceRect();  
-    
+    public readonly RectInt sourceRect;
+
     /// 原图尺寸 (裁边前)
-    Vector2Int sourceSize();
+    public readonly Vector2Int sourceSize;
 
     /// 归一化锚点, 通常 0~1
-    Vector2 pivot();
+    public readonly Vector2 pivot;
+
+    public ScxSprite(string name, RectInt atlasRect, RectInt sourceRect, Vector2Int sourceSize, Vector2 pivot) {
+        this.name = name;
+        this.atlasRect = atlasRect;
+        this.sourceRect = sourceRect;
+        this.sourceSize = sourceSize;
+        this.pivot = pivot;
+    }
     
 }
