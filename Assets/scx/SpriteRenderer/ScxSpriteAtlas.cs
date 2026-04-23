9c70de3196faaad5ea2a01fcb9790ae08f2042e7
scx567888 <scx567888@outlook.com>
2026-04-23 13:25:14 +0800

    dev

---
+++ Assets/scx/SpriteRenderer/ScxSpriteAtlas.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// SCX 图集
+/// </summary>
+public class ScxSpriteAtlas {
+    
+    /// <summary>
+    /// 贴图
+    /// </summary>
+    private Texture2D texture;
+    
+    /// <summary>
+    /// 精灵列表
+    /// </summary>
+    private ScxSprite[] sprites;
+    
+}
