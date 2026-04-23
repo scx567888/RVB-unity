0ce9c8a7d7f50105c52bedff3e2e2aff40578ca5
scx567888 <scx567888@outlook.com>
2026-04-23 14:39:35 +0800

    dev

---
+++ Assets/scx/SpriteRenderer/ScxSpriteRenderer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public sealed class ScxSpriteRenderer {
+    
+    private readonly ScxSpriteAtlas atlas;
+    private readonly float pixelsPerUnit;
+    private readonly Material materialTemplate;
+    private readonly int batchCapacity;
+
+    public ScxSpriteRenderer(ScxSpriteAtlas atlas, float pixelsPerUnit, Material materialTemplate, int batchCapacity) {
+        this.atlas = atlas;
+        this.pixelsPerUnit = pixelsPerUnit;
+        this.materialTemplate = materialTemplate;
+        this.batchCapacity = batchCapacity;
+    }
+    
+}
