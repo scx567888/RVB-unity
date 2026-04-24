cede3c10b1556fcbea9857b6cfcf837a85453370
scx567888 <scx567888@outlook.com>
2026-04-24 14:28:21 +0800

    dev

--- Assets/scx/SpriteRenderer/ScxSpriteAtlas.cs
+++ Assets/scx/SpriteRenderer/ScxSpriteAtlas.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 
-/// SCX 图集
-public sealed class ScxSpriteAtlas {
-    /// 贴图
-    public readonly Texture2D texture;
+namespace scx.SpriteRenderer {
+    /// SCX 图集
+    public sealed class ScxSpriteAtlas {
+        /// 贴图
+        public readonly Texture2D texture;
 
-    /// 精灵列表
-    public readonly ScxSprite[] sprites;
+        /// 精灵列表
+        public readonly ScxSprite[] sprites;
 
-    public ScxSpriteAtlas(Texture2D texture, ScxSprite[] sprites) {
-        this.texture = texture;
-        this.sprites = sprites;
+        public ScxSpriteAtlas(Texture2D texture, ScxSprite[] sprites) {
+            this.texture = texture;
+            this.sprites = sprites;
+        }
     }
 }
