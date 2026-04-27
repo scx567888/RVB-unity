1baf0dadd7b46d0e2ba0d66d7b2ce281699ff575
scx567888 <scx567888@outlook.com>
2026-04-27 09:45:25 +0800

    dev

--- Assets/scx/SpriteRenderer/ScxSpriteRenderUnit.cs
+++ Assets/scx/SpriteRenderer/ScxSpriteRenderUnit.cs
@@ -2,124 +2,80 @@
 
 namespace scx.SpriteRenderer {
     public class ScxSpriteRenderUnit {
+
         private ScxSpriteRenderer spriteRenderer;
-        private ScxSpriteRenderBatch renderBatch;
-        public readonly int batchID;
-        public readonly int index;
+        public ScxSpriteInstanceData instance;
 
-        private ScxSpriteRenderData sprite;
-        private Vector3 position;
-        private Quaternion rotation;
-        private Vector3 scale;
-        private bool visible;
-
-        private Vector3 p0;
-        private Vector3 p1;
-        private Vector3 p2;
-        private Vector3 p3;
-
-        public ScxSpriteRenderUnit(ScxSpriteRenderer spriteRenderer, ScxSpriteRenderBatch renderBatch, int batchID,
-            int index) {
+        public ScxSpriteRenderUnit(ScxSpriteRenderer spriteRenderer, ScxSpriteInstanceData instance) {
             this.spriteRenderer = spriteRenderer;
-            this.renderBatch = renderBatch;
-            this.batchID = batchID;
-            this.index = index;
 
             // 默认初始化第一个 
-            this.sprite = spriteRenderer.getSpriteByIndex(0);
-            this.position = new Vector3(0, 0, 0);
-            this.rotation = new Quaternion(0, 0, 0, 1);
-            this.scale = new Vector3(1, 1, 1);
-            this.visible = false;
-            this.renderBatch.setUVs(this.index, this.sprite.uv0, this.sprite.uv1, this.sprite.uv2, this.sprite.uv3);
+            instance.geometry = this.spriteRenderer.getSpriteGeometryByIndex(0);
+            instance.setVisible(false);
         }
 
         // UV
         public void setFrame(string name) {
-            this.sprite = this.spriteRenderer.getSpriteByName(name);
-            this.renderBatch.setUVs(this.index, sprite.uv0, sprite.uv1, sprite.uv2, sprite.uv3);
-            this.updateUnitVertices();
+            instance.geometry = this.spriteRenderer.getSpriteGeometryByName(name);
         }
 
         // UV
         public void setFrame(int index) {
-            this.sprite = this.spriteRenderer.getSpriteByIndex(index);
-            this.renderBatch.setUVs(this.index, sprite.uv0, sprite.uv1, sprite.uv2, sprite.uv3);
-            this.updateUnitVertices();
+            instance.geometry = this.spriteRenderer.getSpriteGeometryByIndex(index);
         }
 
         // 位置
         public void setPosition(float x, float y, float z) {
-            this.position.Set(x, y, z);
-            this.updateUnitVertices();
+            instance.position.Set(x, y, z);
         }
 
         public Vector3 getPosition() {
-            return this.position;
+            return this.instance.position;
         }
 
         public void translate(float dx, float dy, float dz) {
-            this.position.x += dx;
-            this.position.y += dy;
-            this.position.z += dz;
-            this.updateUnitVertices();
+            instance.position.x += dx;
+            instance.position.y += dy;
+            instance.position.z += dz;
         }
 
         // 旋转
         public void setRotation(float x, float y, float z, float w) {
-            this.rotation.Set(x, y, z, w);
-            this.updateUnitVertices();
+            instance.rotation.Set(x, y, z, w);
         }
 
         public void setRotationFromEuler(float x, float y, float z) {
-            this.rotation = Quaternion.Euler(x, y, z);
-            this.updateUnitVertices();
+            instance.rotation = Quaternion.Euler(x, y, z);
         }
 
         public Quaternion getRotation() {
-            return rotation;
+            return instance.rotation;
         }
 
         public void rotate(float dx, float dy, float dz, float dw) {
-            this.rotation *= new Quaternion(dx, dy, dz, dw);
-            this.updateUnitVertices();
+            instance.rotation *= new Quaternion(dx, dy, dz, dw);
         }
 
         public void rotateFromEuler(float dx, float dy, float dz) {
-            this.rotation *= Quaternion.Euler(dx, dy, dz);
-            this.updateUnitVertices();
+            instance.rotation *= Quaternion.Euler(dx, dy, dz);
         }
 
         // 缩放
         public void setScale(float x, float y, float z) {
-            this.scale.Set(x, y, z);
-            this.updateUnitVertices();
+            instance.scale.Set(x, y, z);
         }
 
         public Vector3 getScale() {
-            return this.scale;
+            return instance.scale;
         }
 
         // 可见性
         public void setVisible(bool visible) {
-            // 如果和之前一样 跳过
-            if (this.visible == visible) {
-                return;
-            }
-
-            this.visible = visible;
-
-            if (this.visible) {
-                this.updateUnitVertices();
-            }
-            else {
-                // 通过将单元的所有顶点塌缩到 0 点(0, 0, 0), 使其在视觉上隐藏/移除
-                this.renderBatch.setPositions(index, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
-            }
+            instance.setVisible(visible);
         }
 
         public bool getVisible() {
-            return visible;
+            return instance.visible;
         }
 
         // 销毁
@@ -127,74 +83,8 @@ namespace scx.SpriteRenderer {
             this.spriteRenderer.destroyUnit(this);
             // 置空 防止后续外部调用
             this.spriteRenderer = null;
-            this.renderBatch = null;
+            this.instance = null;
         }
-
-        private void updateUnitVertices() {
-            if (!this.visible) {
-                return;
-            }
-
-            var qx = rotation.x;
-            var qy = rotation.y;
-            var qz = rotation.z;
-            var qw = rotation.w;
-
-            var x2 = qx + qx;
-            var y2 = qy + qy;
-            var z2 = qz + qz;
-
-            var xx = qx * x2;
-            var xy = qx * y2;
-            var xz = qx * z2;
-            var yy = qy * y2;
-            var yz = qy * z2;
-            var zz = qz * z2;
-            var wx = qw * x2;
-            var wy = qw * y2;
-            var wz = qw * z2;
-
-            var sx = scale.x;
-            var sy = scale.y;
-            var sz = scale.z;
-
-
-            var m00 = (1 - (yy + zz)) * sx;
-            var m01 = (xy + wz) * sx;
-            var m02 = (xz - wy) * sx;
-
-            var m04 = (xy - wz) * sy;
-            var m05 = (1 - (xx + zz)) * sy;
-            var m06 = (yz + wx) * sy;
-
-            var m08 = (xz + wy) * sz;
-            var m09 = (yz - wx) * sz;
-            var m10 = (1 - (xx + yy)) * sz;
-
-            var m12 = position.x;
-            var m13 = position.y;
-            var m14 = position.z;
-
-            var sprite = this.sprite;
-
-            // 更新 positions
-            p0.x = m00 * sprite.p0x + m04 * sprite.p0y + m08 * sprite.p0z + m12;
-            p0.y = m01 * sprite.p0x + m05 * sprite.p0y + m09 * sprite.p0z + m13;
-            p0.z = m02 * sprite.p0x + m06 * sprite.p0y + m10 * sprite.p0z + m14;
-
-            p1.x = m00 * sprite.p1x + m04 * sprite.p1y + m08 * sprite.p1z + m12;
-            p1.y = m01 * sprite.p1x + m05 * sprite.p1y + m09 * sprite.p1z + m13;
-            p1.z = m02 * sprite.p1x + m06 * sprite.p1y + m10 * sprite.p1z + m14;
-
-            p2.x = m00 * sprite.p2x + m04 * sprite.p2y + m08 * sprite.p2z + m12;
-            p2.y = m01 * sprite.p2x + m05 * sprite.p2y + m09 * sprite.p2z + m13;
-            p2.z = m02 * sprite.p2x + m06 * sprite.p2y + m10 * sprite.p2z + m14;
-
-            p3.x = m00 * sprite.p3x + m04 * sprite.p3y + m08 * sprite.p3z + m12;
-            p3.y = m01 * sprite.p3x + m05 * sprite.p3y + m09 * sprite.p3z + m13;
-            p3.z = m02 * sprite.p3x + m06 * sprite.p3y + m10 * sprite.p3z + m14;
-
-            this.renderBatch.setPositions(index, p0, p1, p2, p3);
-        }
+        
     }
 }
