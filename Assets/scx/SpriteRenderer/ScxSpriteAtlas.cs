684b996e3dad22983e69e65e7d996e6eee59c3c3
scx567888 <scx567888@outlook.com>
2026-07-08 14:05:32 +0800

    dev

--- Assets/scx/SpriteRenderer/ScxSpriteAtlas.cs
+++ Assets/scx/SpriteRenderer/ScxSpriteAtlas.cs
@@ -2,11 +2,13 @@
 
 namespace scx.SpriteRenderer {
     /// SCX 图集
+    /// 
+    /// 图集由一张真实贴图和若干图块元数据组成.
     public sealed class ScxSpriteAtlas {
         /// 贴图
         public readonly Texture2D texture;
 
-        /// 精灵列表
+        /// 图块元数据列表
         public readonly ScxSprite[] sprites;
 
         public ScxSpriteAtlas(Texture2D texture, ScxSprite[] sprites) {
