6be2b6280913744bd05ee957166e006ec96b2bef
scx567888 <scx567888@outlook.com>
2026-07-08 14:40:03 +0800

    dev

--- Assets/scx/SpriteRenderer/ScxSprite.cs
+++ Assets/scx/SpriteRenderer/ScxSprite.cs
@@ -5,7 +5,7 @@ namespace scx.SpriteRenderer {
     ///
     /// - 像素坐标 采用左下原点坐标系: 原点在左下角, x 向右, y 向上.
     /// - 归一化坐标 采用左下原点坐标系: 原点在左下角, (0,0) = 左下角, (1,1) = 右上角.
-    /// - 不支持图集打包阶段的旋转.
+    /// - 不支持图集打包阶段的旋转: 图块在图集中的方向必须与原图方向一致.
     public sealed class ScxSprite {
         /// 图块名称
         public readonly string name;
