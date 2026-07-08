83fae7fd230ab4938ea5e41a419b8efd881eb0f8
scx567888 <scx567888@outlook.com>
2026-07-08 15:15:47 +0800

    dev

---
+++ Assets/scx/SpriteRenderer/ScxSpriteAtlasUnitySpriteAtlasLoader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace scx.SpriteRenderer {
+    /// ScxSpriteAtlas 加载器 
+    /// 适用于 Unity 自带的 SpriteAtlas 格式
+    public static class ScxSpriteAtlasUnitySpriteAtlasLoader {
+        /// 将 Unity 自带的 SpriteAtlas 格式 加载为 ScxSpriteAtlas
+        public static ScxSpriteAtlas load(SpriteAtlas unitySpriteAtlas) {
+            if (unitySpriteAtlas == null) {
+                throw new ArgumentNullException(nameof(unitySpriteAtlas));
+            }
+
+            // 1, 读取 unitySpriteAtlas 中所有的 Sprites
+            var unitySprites = new Sprite[unitySpriteAtlas.spriteCount];
+            var actualCount = unitySpriteAtlas.GetSprites(unitySprites);
+
+            if (actualCount == 0) {
+                throw new ArgumentException("unitySpriteAtlas 至少要有一个 sprite.", nameof(unitySpriteAtlas));
+            }
+
+            // 2, 我们采用 第一个 unitySprites 的贴图作为整个贴图.
+            // 这也意味着 我们不支持 unitySpriteAtlas 存在多个 pack
+            var atlasTexture = unitySprites[0].texture;
+
+            // 3, 转换为 ScxSprite
+            var scxSprites = new ScxSprite[actualCount];
+
+            for (var i = 0; i < actualCount; i += 1) {
+                var unitySprite = unitySprites[i];
+
+                // 不支持旋转打包
+                if (unitySprite.packed && unitySprite.packingRotation != SpritePackingRotation.None) {
+                    throw new NotSupportedException(
+                        $"Sprite '{unitySprite.name}' uses packing rotation '{unitySprite.packingRotation}', " +
+                        "but ScxSprite currently does not support rotated sprites.");
+                }
+
+                // 你的系统目前是 quad，不支持 tight packed polygon
+                if (unitySprite.packed && unitySprite.packingMode != SpritePackingMode.Rectangle) {
+                    // throw new NotSupportedException(
+                    // $"Sprite '{unitySprite.name}' uses packing mode '{unitySprite.packingMode}', " +
+                    // "but ScxSprite currently only supports rectangle packing.");
+                }
+
+                // 当前 atlas 贴图中的矩形
+                // 注意：tight packed 时 textureRect 会抛异常，所以前面先拒绝了非 Rectangle
+                var textureRect = unitySprite.textureRect;
+
+                // 原始 Sprite 在原始贴图上的矩形
+                var originalRect = unitySprite.rect;
+
+                // 当前使用矩形相对原始 bounds 的偏移
+                var offset = unitySprite.textureRectOffset;
+
+                var atlasRect = new RectInt(
+                    Mathf.RoundToInt(textureRect.x),
+                    Mathf.RoundToInt(textureRect.y),
+                    Mathf.RoundToInt(textureRect.width),
+                    Mathf.RoundToInt(textureRect.height)
+                );
+
+                var sourceRect = new RectInt(
+                    Mathf.RoundToInt(offset.x),
+                    Mathf.RoundToInt(offset.y),
+                    Mathf.RoundToInt(textureRect.width),
+                    Mathf.RoundToInt(textureRect.height)
+                );
+
+                var sourceSize = new Vector2Int(
+                    Mathf.RoundToInt(originalRect.width),
+                    Mathf.RoundToInt(originalRect.height)
+                );
+                
+                // pivot 像素 -> 归一化 pivot
+                var pivot = new Vector2(
+                    unitySprite.pivot.x / originalRect.width,
+                    unitySprite.pivot.y / originalRect.height
+                );
+
+                scxSprites[i] = new ScxSprite(
+                    unitySprite.name,
+                    atlasRect,
+                    sourceRect,
+                    sourceSize,
+                    pivot
+                );
+            }
+
+            return new ScxSpriteAtlas(atlasTexture, scxSprites);
+        }
+    }
+}
