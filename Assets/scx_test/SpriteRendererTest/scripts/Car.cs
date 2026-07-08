19f4013b5f909c0ed322fecb1231d48c92f5d423
scx567888 <scx567888@outlook.com>
2026-07-08 17:11:03 +0800

    dev

---
+++ Assets/scx_test/SpriteRendererTest/scripts/Car.cs
@@ -0,0 +1,14 @@
+﻿using scx.SpriteRenderer;
+
+public class Car {
+
+    public ScxSpriteRenderUnit renderUnit;
+
+    public int frameIndex;
+
+    public Car(ScxSpriteRenderUnit renderUnit, int frameIndex) {
+        this.renderUnit = renderUnit;
+        this.frameIndex = frameIndex;
+    }
+    
+}
