967c9091f4780250ac3aad8ed17243527afddaf9
scx567888 <scx567888@outlook.com>
2026-07-09 15:55:43 +0800

    dev

--- Assets/scx/SpriteRenderer/ScxSprite.cs
+++ Assets/scx/SpriteRenderer/ScxSprite.cs
@@ -1,39 +1,14 @@
-﻿using UnityEngine;
-
-namespace scx.SpriteRenderer {
+﻿namespace scx.SpriteRenderer {
     /// SCX 图块元数据
     ///
     /// - 像素坐标 采用左下原点坐标系: 原点在左下角, x 向右, y 向上.
     /// - 归一化坐标 采用左下原点坐标系: 原点在左下角, (0,0) = 左下角, (1,1) = 右上角.
     /// - 不支持图集打包阶段的旋转: 图块在图集中的方向必须与原图方向一致.
-    public sealed class ScxSprite {
+    public interface ScxSprite {
         /// 图块名称
-        public readonly string name;
+        public string name();
 
-        /// 图块在图集中的区域 (像素坐标)
-        public readonly RectInt atlasRect;
-
-        /// 图块在原图中的有效区域 (像素坐标)
-        public readonly RectInt sourceRect;
-
-        /// 原图尺寸/裁边前尺寸 (像素单位)
-        public readonly Vector2Int sourceSize;
-
-        /// 基于原图尺寸 sourceSize 的锚点 (归一化坐标)
-        public readonly Vector2 pivot;
-
-        public ScxSprite(
-            string name,
-            RectInt atlasRect,
-            RectInt sourceRect,
-            Vector2Int sourceSize,
-            Vector2 pivot
-        ) {
-            this.name = name;
-            this.atlasRect = atlasRect;
-            this.sourceRect = sourceRect;
-            this.sourceSize = sourceSize;
-            this.pivot = pivot;
-        }
+        /// 创建 ScxSpriteRenderData
+        public ScxSpriteRenderData createSpriteRenderData(int textureWidth, int textureHeight, float pixelsPerUnit);
     }
 }
