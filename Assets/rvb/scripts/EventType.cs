136e07abe67b9a54382c7993279d17edc1944656
scx567888 <scx567888@outlook.com>
2026-07-13 17:15:33 +0800

    dev

---
+++ Assets/rvb/scripts/EventType.cs
@@ -0,0 +1,16 @@
+﻿namespace rvb.scripts {
+    public static class EventType {
+        public static string Red = "Red";
+      public static string RedNum = "RedNum";
+      public static string RessUpdate = "RessUpdate";
+      public static string RoomState = "RoomState";
+      public static string RoomStateStart = "RoomStateStart";
+      public static string RoomStateRun = "RoomStateRun";
+      public static string RoomStateEnd = "RoomStateEnd";
+      public static string NetWorkLoadComplete = "NetWorkLoadComplete";
+      public static string GameModeLoadStart = "GameModeLoadStart";
+      public static string GameModeLoadEnd = "GameModeLoadEnd";
+      public static string UpdateRot= "UpdateRot";
+      public static string OnGameStart = "OnGameStart";
+    }
+}
