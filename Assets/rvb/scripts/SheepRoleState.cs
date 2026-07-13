5cdd1bacccfc0449dbd50fbcadc73337ff9656dd
scx567888 <scx567888@outlook.com>
2026-07-13 16:38:41 +0800

    dev

---
+++ Assets/rvb/scripts/SheepRoleState.cs
@@ -0,0 +1,26 @@
+﻿namespace rvb.scripts {
+    public enum SheepRoleState {
+        In,
+        Spurt,
+        Move,
+        Attack,
+        Dead,
+        Merge,
+        Res,
+        Killer,
+        Boom,
+        SkillBullet,
+        Invincible,
+        Bladestorm,
+        Palm,
+        CallBullets,
+        Buff,
+        Up,
+        Start,
+        Charge,
+        SpinSpurt,
+        SpinAtk,
+        Rigidity,
+        ChargePlus
+    }
+}
