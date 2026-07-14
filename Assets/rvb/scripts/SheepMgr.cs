c50c4573a1600af6031dc2fa139df6045e245826
scx567888 <scx567888@outlook.com>
2026-07-14 17:37:04 +0800

    dev

---
+++ Assets/rvb/scripts/SheepMgr.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+namespace rvb.scripts {
+    public class SheepMgr {
+        public static SheepMgr sheepMgr = new SheepMgr();
+    }
+}
