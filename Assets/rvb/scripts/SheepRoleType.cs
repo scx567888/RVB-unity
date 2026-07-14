accdaee000c5ebfc6163c44be02e08456ed055b7
scx567888 <scx567888@outlook.com>
2026-07-14 13:20:42 +0800

    dev

--- Assets/rvb/scripts/SheepRoleType.cs
+++ Assets/rvb/scripts/SheepRoleType.cs
@@ -1,36 +1,36 @@
 ﻿namespace rvb.scripts {
     public enum SheepRoleType {
         // 此处是攻击模式 不代表真正的单位
-        boss,
+        boss = 0,
 
         // 小兵
-        xiao_bing,
+        xiao_bing = 1,
 
         // 刺客
-        ci_ke,
+        ci_ke = 2,
 
         // 弓箭手
-        gong_jian_shou,
+        gong_jian_shou = 3,
 
         // 盾兵
-        dun_bing,
+        dun_bing = 4,
 
         // 冲锋兵
-        chong_feng_bing,
+        chong_feng_bing = 5,
 
         // 旋风斩
-        xuan_feng_zhan,
+        xuan_feng_zhan = 6,
 
         // 炮车
-        pao_che,
+        pao_che = 7,
 
         // 羊神
-        yang_shen,
+        yang_shen = 8,
 
         // 麒麟
-        qi_lin,
-        buff,
-        tower,
-        loongMiao
+        qi_lin = 9,
+        buff = 10,
+        tower = 11,
+        loongMiao = 12
     }
 }
