030afc76ea4906e9110c10c210c248a9d40146d8
scx567888 <scx567888@outlook.com>
2026-07-14 13:43:16 +0800

    dev

---
+++ Assets/rvb/scripts/SheepRoleAnimType.cs
@@ -0,0 +1,24 @@
+﻿namespace rvb.scripts {
+    public enum SheepRoleAnimType {
+        None = 0,
+        In = 1,
+        Idle = 2,
+        Attack = 3,
+        Dead = 4,
+        Merge = 5,
+        Spurt = 6,
+        Killer = 7,
+        Boom = 8,
+        Invincible = 9,
+        Bladestorm = 10,
+        Palm = 11,
+        CallBullets = 12,
+        Buff = 13,
+        Up = 14,
+        Dead2 = 15,
+        Dead3 = 16,
+        Dead4 = 17,
+        Dead5 = 18,
+        Dead6 = 19
+    }
+}
