91b8a301c6301de2b6b41345ffac042738e70be6
scx567888 <scx567888@outlook.com>
2026-07-14 14:10:12 +0800

    dev

--- Assets/rvb/scripts/SheepCallInfo.cs
+++ Assets/rvb/scripts/SheepCallInfo.cs
@@ -1,12 +1,12 @@
 ﻿namespace rvb.scripts {
     public class SheepCallInfo {
-        public int camp ;
-        public int type ;
-        public int count ;
-        public int frame ;
-        public int items ;
-        public int pets ;
-        public int hasName ;
-        public int count_line ;
+        public int camp;
+        public int type;
+        public int count;
+        public int frame;
+        public int items;
+        public int pets;
+        public int hasName;
+        public int count_line;
     }
 }
