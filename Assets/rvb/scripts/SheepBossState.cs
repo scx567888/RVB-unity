eb3bc3acabb0fd17467059c91cb679e855c1a56f
scx567888 <scx567888@outlook.com>
2026-07-14 13:41:42 +0800

    dev

---
+++ Assets/rvb/scripts/SheepBossState.cs
@@ -0,0 +1,11 @@
+﻿namespace rvb.scripts {
+    public enum SheepBossState {
+        Ready = 0,
+        NomalRun = 1,
+        AwakeRun = 2,
+        BackRun = 3,
+        AwakeAnim = 4,
+        UnAwakeAnim = 5,
+        Dead = 6
+    }
+}
