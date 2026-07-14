0e45bb84852648e2b5c2eccae1cddcdd086e7bb1
scx567888 <scx567888@outlook.com>
2026-07-14 13:23:40 +0800

    dev

--- Assets/rvb/scripts/SheepRoleAtkType.cs
+++ Assets/rvb/scripts/SheepRoleAtkType.cs
@@ -1,8 +1,8 @@
 ﻿namespace rvb.scripts {
     public enum SheepRoleAtkType {
-        Nearest,
-        Farthest,
-        Random,
-        Throw
+        Nearest = 0,
+        Farthest = 1,
+        Random = 2,
+        Throw = 3
     }
 }
