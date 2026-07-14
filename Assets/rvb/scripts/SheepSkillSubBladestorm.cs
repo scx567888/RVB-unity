a0cde2e2dd10247bd423f77f02dc7dacb73f9cc4
scx567888 <scx567888@outlook.com>
2026-07-14 13:52:20 +0800

    dev

---
+++ Assets/rvb/scripts/SheepSkillSubBladestorm.cs
@@ -0,0 +1,15 @@
+﻿namespace rvb.scripts {
+    public class SheepSkillSubBladestorm {
+        public int id;
+        public int name;
+        public int endFrame;
+        public int spiltRadiusBet;
+        public int atkFindR;
+        public int atkBet;
+        public int findMoveFrame;
+        public int atkFrames;
+        public int parryPercent;
+        public int speed;
+        public int findR;
+    }
+}
