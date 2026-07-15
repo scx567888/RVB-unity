6b7cf3b88d4581990549aa766ec06e2af732e2ba
scx567888 <scx567888@outlook.com>
2026-07-15 17:19:22 +0800

    dev

--- Assets/rvb/scripts/BullteCreate.cs
+++ Assets/rvb/scripts/BullteCreate.cs
@@ -1,23 +1,25 @@
 ﻿namespace rvb.scripts {
     public class BullteCreate {
-       public PetView view_pet;
-       public int bulletId;
-       public PetView view_tar_pet;
-       public Info info;
-    }
+        public PetView view_pet;
+        public int bulletId;
+        public PetView view_tar_pet;
+        public Info info;
 
-    public class Info {
-        public int startX;
-        public int startY;
-        public int startZ;
-        public int endX;
-        public int endY;
-        public int endZ;
-        public int dirX;
-        public int dirY;
-        public int dirZ;
-        public int angle;
-        public SheepCamp camp;
-        public float atk;
+        public class Info {
+            public float startX;
+            public float startY;
+            public float startZ;
+            public float endX;
+            public float endY;
+            public float endZ;
+            public float dirX;
+            public float dirY;
+            public float dirZ;
+            public float angle;
+            public SheepCamp camp;
+            public float atk;
+            public bool hasStart;
+            public bool hasEnd;
+        }
     }
 }
