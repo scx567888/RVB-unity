9760b2cafbefc7696cbd20f71231aec0e4462143
scx567888 <scx567888@outlook.com>
2026-07-16 18:33:54 +0800

    dev

--- Assets/rvb/scripts/EventBus.cs
+++ Assets/rvb/scripts/EventBus.cs
@@ -1,6 +1,6 @@
 ﻿namespace rvb.scripts {
     public class EventBus {
-        public static EventBus eventBus;
+        public static EventBus eventBus=new EventBus();
 
         public void emit(string name,object value) {
             
