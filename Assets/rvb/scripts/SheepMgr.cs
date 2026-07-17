28a44e3f9db3cfae0e002d0660ea39c113282a58
scx567888 <scx567888@outlook.com>
2026-07-17 15:53:10 +0800

    dev

--- Assets/rvb/scripts/SheepMgr.cs
+++ Assets/rvb/scripts/SheepMgr.cs
@@ -1955,57 +1955,54 @@ namespace rvb.scripts {
                 petSkin.impulseY = 0f;
             }
         }
-// todo
-        public void update_role_state_start(PetView petSkin, bool logicTick, float deltaSeconds) {
-            if (state == SheepRoomState.Start) {
-                if (!logicTick) return;
 
-                float distance = Util.dis(
-                    petSkin.posX,
-                    petSkin.posY,
-                    petSkin.tarPosX,
-                    petSkin.tarPosY
-                );
-                float speed = 3f * petSkin.conf.runSpeed;
-                if (distance > speed * deltaSeconds) {
-                    float[] direction = Util.dirTarByPos(petSkin, petSkin.tarPosX, petSkin.tarPosY);
-                    petSkin.logicMove(
-                        petSkin.posX + direction[0] * speed * deltaSeconds,
-                        petSkin.posY + direction[1] * speed * deltaSeconds
-                    );
+        public void update_role_state_start(PetView petSkin, bool t, float s) {
+            if (this.state == SheepRoomState.Start) {
+                if (t) {
+                    var t2 = petSkin.posX;
+                    var i = petSkin.posY;
+                    var o = petSkin.tarPosX;
+                    var l = petSkin.tarPosY;
+                    var n = Util.dis(t2, i, o, l);
+                    var r = 3 * petSkin.conf.runSpeed;
+                    if (n > r * s) {
+                        var ddd = Util.dirTarByPos(petSkin, petSkin.tarPosX, petSkin.tarPosY);
+                        var t3 = ddd[0];
+                        var i3 = ddd[1];
+                        var o3 = new Vector3(){x= petSkin.posX, y= petSkin.posY};
+                        var l3 = new Vector3(){x= t3 * r * s, y= i3 * r * s};
+                        var n3 = new Vector3(){x= o3.x + l3.x, y= o3.y + l3.y};
+                        petSkin.logicMove(n3.x, n3.y);
+                    } else {
+                        petSkin.logicMove(o, l);
+                    }
                 }
-                else {
-                    petSkin.logicMove(petSkin.tarPosX, petSkin.tarPosY);
-                }
-
-                return;
-            }
-
-            if (petSkin.conf.skillSpurt != 0) {
-                SheepSkill skill = SheepSkill.getById(petSkin.conf.skillSpurt);
-                if (skill.skillType == SheepSkillType.Charge) {
+            } else if (petSkin.conf.skillSpurt!=0) {
+                var t1 = SheepSkill.getById(petSkin.conf.skillSpurt);
+                if (t1.skillType == SheepSkillType.Charge) {
                     petSkin.state = SheepRoleState.Charge;
                     petSkin.subState = SheepRoleSubState.Spurt;
                     petSkin.animType = SheepRoleAnimType.Spurt;
-                }
-                else if (skill.skillType == SheepSkillType.SpinSpurt) {
+                } else if (t1.skillType == SheepSkillType.SpinSpurt) {
                     petSkin.state = SheepRoleState.SpinSpurt;
                     petSkin.animType = SheepRoleAnimType.Attack;
-                }
-                else {
+                } else {
                     petSkin.state = SheepRoleState.Spurt;
                     petSkin.subState = SheepRoleSubState.Spurt;
-                    petSkin.animType = petSkin.conf.isSpurtAnim
-                        ? SheepRoleAnimType.Spurt
-                        : SheepRoleAnimType.Idle;
+                    if (petSkin.conf.isSpurtAnim) {
+                        petSkin.animType = SheepRoleAnimType.Spurt;
+                    } else {
+                        petSkin.animType = SheepRoleAnimType.Idle;
+                    }
                 }
-            }
-            else {
+            } else {
                 petSkin.state = SheepRoleState.Spurt;
                 petSkin.subState = SheepRoleSubState.Spurt;
-                petSkin.animType = petSkin.conf.isSpurtAnim
-                    ? SheepRoleAnimType.Spurt
-                    : SheepRoleAnimType.Idle;
+                if (petSkin.conf.isSpurtAnim) {
+                    petSkin.animType = SheepRoleAnimType.Spurt;
+                } else {
+                    petSkin.animType = SheepRoleAnimType.Idle;
+                }
             }
         }
 // todo
