e6f806cd07631557d148abf47a73b95e2ce6c6dd
scx567888 <scx567888@outlook.com>
2026-07-21 15:46:22 +0800

    dev

--- Assets/sheep/SheepGame.cs
+++ Assets/sheep/SheepGame.cs
@@ -1,29 +1,71 @@
+using rvb.utils;
+using scx.SpriteRenderer;
 using UnityEngine;
 
 namespace sheep {
     public class SheepGame : MonoBehaviour {
+        // ********************* 逻辑帧相关 *********************
+
         // 逻辑帧率
         public int tickRate = 30;
-
-        // 逻辑帧率 耗时
-        private double tickInterval => 1.0 / tickRate;
 
         // 逻辑帧率累加器
         private double tickAccumulator = 0;
 
+        // ********************* 资源相关 *********************
+
+        // 渲染器资源
+        public Texture2D texture;
+        public TextAsset json;
+        public Material mainMaterial;
+
+        // ********************* 渲染器相关 *********************
+
+        private ScxSpriteRenderer scxSpriteRenderer;
+        private ScxSpriteRenderUnit scxSpriteRenderUnit;
+
+        // ********************* 主逻辑相关 *********************
+
         private SheepWorld sheepWorld;
 
         void Start() {
+            // 创建渲染器
+            var loadRoleResult = SheepSpriteAtlasLoader.loadRole(texture, json.text);
+            this.scxSpriteRenderer = new ScxSpriteRenderer(
+                loadRoleResult.spriteAtlas,
+                100,
+                mainMaterial,
+                100
+            );
+            this.scxSpriteRenderUnit = scxSpriteRenderer.createUnit();
+            this.scxSpriteRenderUnit.setFrame(0);
+
+            // 初始化主逻辑
             sheepWorld = new SheepWorld();
         }
 
         void Update() {
+            // 计算 tick 帧 时长
+            var tickInterval = 1.0 / tickRate;
+            
+            // 累加 tick 帧 计数
             tickAccumulator += Time.deltaTime;
 
+            // 调度逻辑帧
             while (tickAccumulator >= tickInterval) {
                 sheepWorld.tick();
                 tickAccumulator -= tickInterval;
             }
+
+            // 渲染
+            render();
+        }
+
+
+        void render() {
+            var p = sheepWorld.position;
+            scxSpriteRenderUnit.setPosition(p.x, p.y, p.z);
+            scxSpriteRenderer.update();
         }
     }
 }
--- Assets/sheep/SheepWorld.cs
+++ Assets/sheep/SheepWorld.cs
@@ -2,10 +2,18 @@
 
 namespace sheep {
     public class SheepWorld {
-        
+        public Vector3 position = new Vector3();
+        public int a = 1;
+
         public void tick() {
-            Debug.Log("tick");
+            if (position.x > 10) {
+                a = -1;
+            }
+            else if (position.x < -10) {
+                a = 1;
+            }
+
+            position += new Vector3(0.2f * a, 0, 0);
         }
-        
     }
 }
