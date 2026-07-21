c3ab368db2a1fb3d5325460b4874fca48f313976
scx567888 <scx567888@outlook.com>
2026-07-21 15:26:07 +0800

    dev

--- Assets/sheep/SheepGame.cs
+++ Assets/sheep/SheepGame.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 namespace sheep {
     public class SheepGame : MonoBehaviour {
-        private int tick;
+        // 逻辑帧率
+        public int tickRate = 30;
+
+        // 逻辑帧率 耗时
+        private double tickInterval => 1.0 / tickRate;
+
+        // 逻辑帧率累加器
+        private double tickAccumulator = 0;
+
         private SheepWorld sheepWorld;
 
         void Start() {
@@ -10,7 +18,12 @@ namespace sheep {
         }
 
         void Update() {
-            sheepWorld.tick();
+            tickAccumulator += Time.deltaTime;
+
+            while (tickAccumulator >= tickInterval) {
+                sheepWorld.tick();
+                tickAccumulator -= tickInterval;
+            }
         }
     }
 }
