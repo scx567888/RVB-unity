440a679ecdf3e3380f27c6862f10ad59eeb53175
scx567888 <scx567888@outlook.com>
2026-07-23 12:53:05 +0800

    dev

--- Assets/scx/GridMap/GridCell.cs
+++ Assets/scx/GridMap/GridCell.cs
@@ -1,45 +1,46 @@
-﻿/**
+﻿namespace scx.GridMap {
+    /**
  * 表示 GridMap 中的一个单元, 可以继承以实现更多功能
  */
-public class GridCell {
+    public class GridCell {
 
-    /**
+        /**
      *  自己所在的 列 (Grid 坐标系)
      *  @type {number}
      */
-    public readonly int gridX;
+        public readonly int gridX;
 
-    /**
+        /**
      * 自己所在的 行 (Grid 坐标系)
      *  @type {number}
      */
-    public readonly int gridY;
+        public readonly int gridY;
 
-    /**
+        /**
      * 格子起始 X 坐标 (世界坐标系)
      *  @type {number}
      */
-    public readonly float worldStartX;
+        public readonly float worldStartX;
 
-    /**
+        /**
      * 格子起始 Y 坐标 (世界坐标系)
      *  @type {number}
      */
-    public readonly float worldStartY;
+        public readonly float worldStartY;
 
-    /**
+        /**
      * 格子结束 X 坐标 (世界坐标系)
      *  @type {number}
      */
-    public readonly float worldEndX;
+        public readonly float worldEndX;
 
-    /**
+        /**
      * 格子结束 Y 坐标 (世界坐标系)
      *  @type {number}
      */
-    public readonly float worldEndY;
+        public readonly float worldEndY;
 
-    /**
+        /**
      * @param {number} gridX
      * @param {number} gridY
      * @param {number} worldStartX
@@ -47,13 +48,14 @@ public class GridCell {
      * @param {number} worldEndX
      * @param {number} worldEndY
      */
-    public GridCell(int gridX, int gridY, float worldStartX, float worldStartY, float worldEndX, float worldEndY) {
-        this.gridX = gridX;
-        this.gridY = gridY;
-        this.worldStartX = worldStartX;
-        this.worldStartY = worldStartY;
-        this.worldEndX = worldEndX;
-        this.worldEndY = worldEndY;
-    }
+        public GridCell(int gridX, int gridY, float worldStartX, float worldStartY, float worldEndX, float worldEndY) {
+            this.gridX = gridX;
+            this.gridY = gridY;
+            this.worldStartX = worldStartX;
+            this.worldStartY = worldStartY;
+            this.worldEndX = worldEndX;
+            this.worldEndY = worldEndY;
+        }
 
+    }
 }
