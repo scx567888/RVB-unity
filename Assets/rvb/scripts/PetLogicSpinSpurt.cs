bc801a115860970c7cae437582b2d2369f020e38
scx567888 <scx567888@outlook.com>
2026-07-27 18:21:56 +0800

    dev

--- Assets/rvb/scripts/PetLogicSpinSpurt.cs
+++ Assets/rvb/scripts/PetLogicSpinSpurt.cs
@@ -3,8 +3,41 @@
 namespace rvb.scripts {
     public class PetLogicSpinSpurt : PetLogic{
         public static readonly PetLogicSpinSpurt  Instance = new ();
-        public void tick(PetView petView, SheepMgr sheepMgr, bool isLogicFrame) {
-            Debug.Log(petView.state.ToString());
+        public void tick(PetView e, SheepMgr sheepMgr, bool t) {
+            if (!t) {
+                return;
+            }
+
+            var o = e.posX;
+            var l = e.posY;
+            (int n, int r) = sheepMgr.getXnYn(o, l);
+            if (e.camp == SheepCamp.Red && e.posX > e.conf.runEndX ||
+                e.camp == SheepCamp.Blue && e.posX < -e.conf.runEndX) {
+                e.state = SheepRoleState.Boom;
+                e.subState = SheepRoleSubState.Boom;
+                var t1 = SheepSkillSubSpinSpurt.getById(e.conf.skillSpurt);
+                var i1 = SheepSkillSubBoom.getById(t1.endSkill);
+                if (i1.isAnim != 0) {
+                    e.animType = SheepRoleAnimType.Boom;
+                }
+                else {
+                    e.animType = SheepRoleAnimType.Idle;
+                }
+
+                e.readySkillId = i1.id;
+            }
+            else {
+                sheepMgr.moveTar(e, null, t);
+                sheepMgr.forNearBlocksByAckView(e, n, r, e.conf.findR,
+                    t2 => {
+                        if (t2.isDie || t2.camp == e.camp || !sheepMgr.isCanAckByRole(e, t2)) {
+                            return false;
+                        }
+
+                        sheepMgr.ackTar(e, t2);
+                        return false;
+                    });
+            }
         }
     }
 }
--- Assets/rvb/scripts/SheepMgr.cs
+++ Assets/rvb/scripts/SheepMgr.cs
@@ -1560,40 +1560,7 @@ namespace rvb.scripts {
         }
 
         public void update_role_state_spinspurt(PetView e, bool t) {
-            if (!t) {
-                return;
-            }
-
-            var o = e.posX;
-            var l = e.posY;
-            (int n, int r) = getXnYn(o, l);
-            if (e.camp == SheepCamp.Red && e.posX > e.conf.runEndX ||
-                e.camp == SheepCamp.Blue && e.posX < -e.conf.runEndX) {
-                e.state = SheepRoleState.Boom;
-                e.subState = SheepRoleSubState.Boom;
-                var t1 = SheepSkillSubSpinSpurt.getById(e.conf.skillSpurt);
-                var i1 = SheepSkillSubBoom.getById(t1.endSkill);
-                if (i1.isAnim != 0) {
-                    e.animType = SheepRoleAnimType.Boom;
-                }
-                else {
-                    e.animType = SheepRoleAnimType.Idle;
-                }
-
-                e.readySkillId = i1.id;
-            }
-            else {
-                moveTar(e, null, t);
-                forNearBlocksByAckView(e, n, r, e.conf.findR,
-                    t2 => {
-                        if (t2.isDie || t2.camp == e.camp || !isCanAckByRole(e, t2)) {
-                            return false;
-                        }
-
-                        ackTar(e, t2);
-                        return false;
-                    });
-            }
+           
         }
 
         public void update_role_state_spurt(PetView e, bool t) {
