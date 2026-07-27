509621d4d5f733d34fb6702c86bacd2fe9253ff4
scx567888 <scx567888@outlook.com>
2026-07-27 18:55:51 +0800

    dev

--- Assets/rvb/scripts/SheepConfig.cs
+++ Assets/rvb/scripts/SheepConfig.cs
@@ -143,7 +143,7 @@
             counterHpRatio = 0.3f,
             counterTime = 120,
             // WarmUpID = 22,
-            WarmUpID = 7,
+            WarmUpID = 20,
             beheadLine = 8000,
             startMaxTime = 300,
 
