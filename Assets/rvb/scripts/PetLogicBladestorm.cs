66a7cfab88c86b3eebc110d4cfb89f793e6ce9c1
scx567888 <scx567888@outlook.com>
2026-07-28 13:12:59 +0800

    dev

--- Assets/rvb/scripts/PetLogicBladestorm.cs
+++ Assets/rvb/scripts/PetLogicBladestorm.cs
@@ -2,13 +2,19 @@
 
 namespace rvb.scripts {
     public class PetLogicBladestorm : PetLogic{
+        private static readonly int LOOP_FRAME = 4;
         public static readonly PetLogicBladestorm  Instance = new ();
-        public void tick(PetView pet, SheepMgr sheepMgr, bool isLogicFrame) {
+        public void tick(PetView pet, SheepMgr sheepMgr) {
+            // 每四个逻辑帧 (action) 执行一次
+            var shouldExecute = pet.frame % LOOP_FRAME == LOOP_FRAME - 1;
+            if (!shouldExecute) {
+                return;
+            }
             var i = SheepMgr.FixedDeltaTime;
             var s = pet.animFrame;
             var o = SheepSkill.getById(pet.readySkillId);
             var l = SheepSkillSubBladestorm.getById(o.id);
-            if (isLogicFrame) {
+            if (shouldExecute) {
                 var fff = sheepMgr.findTar(pet, l.findR);
                 var t1 = fff.atkTar;
                 var s1 = fff.moveTar;
