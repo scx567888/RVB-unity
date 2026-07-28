43a4910ba3afea42db6058191c2e4a2aab6c338c
scx567888 <scx567888@outlook.com>
2026-07-28 12:50:16 +0800

    dev

--- Assets/rvb/scripts/PetLogicStart.cs
+++ Assets/rvb/scripts/PetLogicStart.cs
@@ -1,35 +1,33 @@
 ﻿using UnityEngine;
 
 namespace rvb.scripts {
-    public class PetLogicStart : PetLogic{
-        
-        public static readonly PetLogicStart  Instance = new PetLogicStart();
-        
+    public class PetLogicStart : PetLogic {
+        public static readonly PetLogicStart Instance = new PetLogicStart();
+
         public void tick(PetView pet, SheepMgr sheepMgr, bool t) {
             if (!t) {
                 return;
             }
 
             if (sheepMgr.state == SheepRoomState.Start) {
-                if (t) {
-                    var t2 = pet.posX;
-                    var i = pet.posY;
-                    var o = pet.tarPosX;
-                    var l = pet.tarPosY;
-                    var n = SheepMgr.dis(t2, i, o, l);
-                    var r = 3 * pet.conf.runSpeed;
-                    if (n > r * SheepMgr.FixedDeltaTime) {
-                        var ddd = SheepMgr.dirTarByPos(pet, pet.tarPosX, pet.tarPosY);
-                        var t3 = ddd[0];
-                        var i3 = ddd[1];
-                        var o3 = new Vector3() { x = pet.posX, y = pet.posY };
-                        var l3 = new Vector3() { x = t3 * r * SheepMgr.FixedDeltaTime, y = i3 * r * SheepMgr.FixedDeltaTime };
-                        var n3 = new Vector3() { x = o3.x + l3.x, y = o3.y + l3.y };
-                        pet.logicMove(n3.x, n3.y);
-                    }
-                    else {
-                        pet.logicMove(o, l);
-                    }
+                var t2 = pet.posX;
+                var i = pet.posY;
+                var o = pet.tarPosX;
+                var l = pet.tarPosY;
+                var n = SheepMgr.dis(t2, i, o, l);
+                var r = 3 * pet.conf.runSpeed;
+                if (n > r * SheepMgr.FixedDeltaTime) {
+                    var ddd = SheepMgr.dirTarByPos(pet, pet.tarPosX, pet.tarPosY);
+                    var t3 = ddd[0];
+                    var i3 = ddd[1];
+                    var o3 = new Vector3() { x = pet.posX, y = pet.posY };
+                    var l3 = new Vector3()
+                        { x = t3 * r * SheepMgr.FixedDeltaTime, y = i3 * r * SheepMgr.FixedDeltaTime };
+                    var n3 = new Vector3() { x = o3.x + l3.x, y = o3.y + l3.y };
+                    pet.logicMove(n3.x, n3.y);
+                }
+                else {
+                    pet.logicMove(o, l);
                 }
             }
             else if (pet.conf.skillSpurt != 0) {
--- Assets/rvb/scripts/SheepSkillType.cs
+++ Assets/rvb/scripts/SheepSkillType.cs
@@ -1,19 +1,19 @@
 ﻿namespace rvb.scripts {
     public enum SheepSkillType {
-        Boom = 1,
-        Killer = 2,
-        Bullet = 3,
-        Bladestorm = 4,
-        Invincible = 5,
-        ForceState = 6,
-        Palm = 7,
-        CallBullets = 8,
-        Buff = 9,
-        Charge = 10,
-        Destruction = 11,
-        SpinAtk = 12,
-        SpinSpurt = 13,
-        Rigidity = 14,
-        ChargePlus = 15
+        Boom = 1,          // 爆炸
+        Killer = 2,        // 刺杀 / 处决
+        Bullet = 3,        // 子弹攻击
+        Bladestorm = 4,    // 剑刃风暴 / 旋风斩
+        Invincible = 5,    // 无敌
+        ForceState = 6,    // 强制状态切换
+        Palm = 7,          // 掌击
+        CallBullets = 8,   // 召唤子弹 / 批量生成子弹
+        Buff = 9,          // 增益效果
+        Charge = 10,       // 冲锋
+        Destruction = 11,  // 毁灭 / 摧毁
+        SpinAtk = 12,      // 旋转攻击
+        SpinSpurt = 13,    // 旋转冲刺
+        Rigidity = 14,     // 僵直 / 硬直
+        ChargePlus = 15    // 强化冲锋
     }
 }
