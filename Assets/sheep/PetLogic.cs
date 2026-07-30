6d77e68465d6e6c653c3980750c7763b0299f9ba
scx567888 <scx567888@outlook.com>
2026-07-30 14:33:14 +0800

    dev

---
+++ Assets/sheep/PetLogic.cs
@@ -0,0 +1,12 @@
+﻿namespace sheep {
+    // 测试
+    public class PetLogic {
+        public static PetLogic INSTANCE = new PetLogic();
+
+        public void tick(Pet pet, SheepWorld sheepWorld) {
+            pet.moveMode = PetMoveMode.TARGET;
+            pet.targetX = sheepWorld.bossX;
+            pet.targetY = sheepWorld.bossY;
+        }
+    }
+}
