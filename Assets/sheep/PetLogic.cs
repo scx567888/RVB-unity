704f90dcc5f0c832e44b53f0e5d1d1ed66fd0d1d
scx567888 <scx567888@outlook.com>
2026-07-30 15:34:30 +0800

    dev

--- Assets/sheep/PetLogic.cs
+++ Assets/sheep/PetLogic.cs
@@ -4,9 +4,16 @@
         public static PetLogic INSTANCE = new PetLogic();
 
         public void tick(Pet pet, SheepWorld sheepWorld) {
-            pet.moveMode = PetMoveMode.TARGET;
-            pet.targetX = sheepWorld.bossX;
-            pet.targetY = sheepWorld.bossY;
+            if (pet.id % 2 == 0) {
+                pet.moveMode = PetMoveMode.TARGET;
+                pet.targetX = sheepWorld.bossX;
+                pet.targetY = sheepWorld.bossY;
+            }
+            else {
+                pet.moveMode = PetMoveMode.TARGET;
+                pet.targetX = sheepWorld.boss1X;
+                pet.targetY = sheepWorld.boss1Y;
+            }
         }
     }
 }
