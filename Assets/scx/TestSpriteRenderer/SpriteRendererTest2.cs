108a00d6de047580697fadaf1edd74cc1abe2e61
scx567888 <scx567888@outlook.com>
2026-08-07 16:17:23 +0800

    dev

--- Assets/scx/TestSpriteRenderer/SpriteRendererTest2.cs
+++ Assets/scx/TestSpriteRenderer/SpriteRendererTest2.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using scx.SpriteRenderer;
 using UnityEngine;
 using UnityEngine.U2D;
@@ -15,32 +14,70 @@ namespace scx.SpriteRendererTest {
 
         // 渲染器
         private ScxSpriteRenderer scxSpriteRenderer;
+        
+        public int line_w = 24;
+        public int line_h = 24;
+        public float d = 0.5f;
+        public Color32 color0 = new Color32(236, 248, 251, 255);
+        public Color32 color1 = new Color32(21, 12, 10, 255);
+        public float randomOffset = 0.02f;
 
-        // 汽车列表
-        private List<ScxSpriteRenderUnit> units;
+        // 单元列表
+        private ScxSpriteRenderUnit[,] units;
 
         void Start() {
             // 创建渲染器
             var scxSpriteAtlas = ScxSpriteAtlasUnitySpriteAtlasLoader.load(this.unitySpriteAtlas);
-            this.scxSpriteRenderer = new ScxSpriteRenderer(scxSpriteAtlas, 300, mainMaterial, 1000);
+            this.scxSpriteRenderer = new ScxSpriteRenderer(scxSpriteAtlas, 100, mainMaterial, 1000);
 
             this.scxSpriteRenderer.setParent(this.gameObject);
+            
+            units=new ScxSpriteRenderUnit[line_w, line_h];
+            
+            var w = line_w * d;
+            var h = line_h * d;
 
-            // 创建单元
-            this.units = new List<ScxSpriteRenderUnit>();
-            for (var j = 0; j < 5000; j++) {
-                var spriteRenderUnit = this.scxSpriteRenderer.createUnit();
-                spriteRenderUnit.setVisible(true);
-                spriteRenderUnit.setPosition(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
-                spriteRenderUnit.setFrame(0);
-                this.units.Add(spriteRenderUnit);
+            
+
+            for (int i = 0; i < line_w; i++) {
+                for (int j = 0; j < line_h; j++) {
+                    var spriteRenderUnit = this.scxSpriteRenderer.createUnit();
+
+                    spriteRenderUnit.setVisible(true);
+                    spriteRenderUnit.setFrame(0);
+                    
+                    var x =
+                        (i + 0.5f) * d
+                        - w / 2
+                        + Random.Range(-randomOffset, randomOffset);
+
+                    var y =
+                        (j + 0.5f) * d
+                        - h / 2
+                        + Random.Range(-randomOffset, randomOffset);
+                    spriteRenderUnit.setPosition(x,y,0);
+                    units[i, j] = spriteRenderUnit;
+                }
             }
+            
+            // 着色
+            for (int i = 0; i < line_w; i++) {
+                for (int j = 0; j < line_h; j++) {
+                    var spriteRenderUnit = units[i, j];
+                    
+                    if (i < line_h / 2) {
+                        spriteRenderUnit.setColor(color0);
+                    }
+                    else {
+                        spriteRenderUnit.setColor(color1);
+                    }
+                    
+                }
+            }
+
         }
 
         void Update() {
-            foreach (var unit in this.units) {
-                unit.setColor(Color.red);
-            }
 
             this.scxSpriteRenderer.update();
         }
