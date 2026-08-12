d77e81aba083cd3b34a492aab51703955fe6ed21
scx567888 <scx567888@outlook.com>
2026-08-12 12:15:49 +0800

    依赖项精简 (#27)

---
+++ Assets/car/CarThirdPersonCamera.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class CarThirdPersonCamera : MonoBehaviour {
+
+    public Transform target;
+
+    public float distance = 6f;
+    public float height = 3f;
+
+    public float positionSpeed = 8f;
+    public float rotationSpeed = 8f;
+
+    void LateUpdate() {
+
+        if (target == null) {
+            return;
+        }
+
+        // 只取汽车在水平面上的朝向
+        // 避免车上坡/翻滚时摄像机也跟着翻
+        var forward = target.forward;
+        forward.y = 0;
+
+        if (forward.sqrMagnitude < 0.001f) {
+            forward = Vector3.forward;
+        }
+
+        forward.Normalize();
+
+        // 摄像机目标位置：
+        // 汽车后面 distance
+        // 上面 height
+        var targetPosition =
+            target.position
+            - forward * distance
+            + Vector3.up * height;
+
+        // 平滑跟随位置
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPosition,
+            positionSpeed * Time.deltaTime
+        );
+
+        // 看向汽车稍微靠上的位置
+        var lookTarget =
+            target.position + Vector3.up * 1f;
+
+        var targetRotation =
+            Quaternion.LookRotation(
+                lookTarget - transform.position,
+                Vector3.up
+            );
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            rotationSpeed * Time.deltaTime
+        );
+    }
+}
---
+++ Assets/car/WheelColliderTest.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class WheelColliderTest : MonoBehaviour {
+
+    public WheelCollider FL;
+    public WheelCollider FR;
+    public WheelCollider RL;
+    public WheelCollider RR;
+
+    public GameObject FLObject;
+    public GameObject FRObject;
+    public GameObject RLObject;
+    public GameObject RRObject;
+
+    public float motorTorque = 1500f;
+    public float steerAngle = 30f;
+    public float brakeTorque = 3000f;
+
+    void FixedUpdate() {
+
+        float motor = 0;
+        float steer = 0;
+
+        if (Input.GetKey(KeyCode.W))
+            motor = motorTorque;
+
+        if (Input.GetKey(KeyCode.S))
+            motor = -motorTorque;
+
+        if (Input.GetKey(KeyCode.A))
+            steer = -steerAngle;
+
+        if (Input.GetKey(KeyCode.D))
+            steer = steerAngle;
+
+        RL.motorTorque = motor;
+        RR.motorTorque = motor;
+
+        FL.steerAngle = steer;
+        FR.steerAngle = steer;
+
+        float brake =
+            Input.GetKey(KeyCode.Space)
+                ? brakeTorque
+                : 0;
+
+        FL.brakeTorque = brake;
+        FR.brakeTorque = brake;
+        RL.brakeTorque = brake;
+        RR.brakeTorque = brake;
+    }
+
+    void Update() {
+        syncWheel(FL, FLObject);
+        syncWheel(FR, FRObject);
+        syncWheel(RL, RLObject);
+        syncWheel(RR, RRObject);
+    }
+
+    void syncWheel(
+        WheelCollider wheel,
+        GameObject obj
+    ) {
+        wheel.GetWorldPose(
+            out var position,
+            out var rotation
+        );
+
+        obj.transform.position = position;
+        // 修正 Cylinder 自身轴向
+        obj.transform.rotation =
+            rotation * Quaternion.Euler(0, 0, 90);
+    }
+}
