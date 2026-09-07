6dee7f5859c9b5ef0f567eb77054c7142794ca24
scx567888 <scx567888@outlook.com>
2026-09-07 15:36:29 +0800

    dev

--- Assets/car/WheelCollider.cs
+++ Assets/car/WheelCollider.cs
@@ -1,74 +1,79 @@
 ﻿using UnityEngine;
 
-public class WheelColliderTest : MonoBehaviour {
+namespace car
+{
+    public class WheelColliderTest : MonoBehaviour
+    {
+        public WheelCollider FL;
+        public WheelCollider FR;
+        public WheelCollider RL;
+        public WheelCollider RR;
 
-    public WheelCollider FL;
-    public WheelCollider FR;
-    public WheelCollider RL;
-    public WheelCollider RR;
+        public GameObject FLObject;
+        public GameObject FRObject;
+        public GameObject RLObject;
+        public GameObject RRObject;
 
-    public GameObject FLObject;
-    public GameObject FRObject;
-    public GameObject RLObject;
-    public GameObject RRObject;
+        public float motorTorque = 1500f;
+        public float steerAngle = 30f;
+        public float brakeTorque = 3000f;
 
-    public float motorTorque = 1500f;
-    public float steerAngle = 30f;
-    public float brakeTorque = 3000f;
+        void FixedUpdate()
+        {
+            float motor = 0;
+            float steer = 0;
 
-    void FixedUpdate() {
+            if (Input.GetKey(KeyCode.W))
+                motor = motorTorque;
 
-        float motor = 0;
-        float steer = 0;
+            if (Input.GetKey(KeyCode.S))
+                motor = -motorTorque;
 
-        if (Input.GetKey(KeyCode.W))
-            motor = motorTorque;
+            if (Input.GetKey(KeyCode.A))
+                steer = -steerAngle;
 
-        if (Input.GetKey(KeyCode.S))
-            motor = -motorTorque;
+            if (Input.GetKey(KeyCode.D))
+                steer = steerAngle;
 
-        if (Input.GetKey(KeyCode.A))
-            steer = -steerAngle;
+            RL.motorTorque = motor;
+            RR.motorTorque = motor;
 
-        if (Input.GetKey(KeyCode.D))
-            steer = steerAngle;
+            FL.steerAngle = steer;
+            FR.steerAngle = steer;
 
-        RL.motorTorque = motor;
-        RR.motorTorque = motor;
+            float brake =
+                Input.GetKey(KeyCode.Space)
+                    ? brakeTorque
+                    : 0;
 
-        FL.steerAngle = steer;
-        FR.steerAngle = steer;
+            FL.brakeTorque = brake;
+            FR.brakeTorque = brake;
+            RL.brakeTorque = brake;
+            RR.brakeTorque = brake;
+        }
 
-        float brake =
-            Input.GetKey(KeyCode.Space)
-                ? brakeTorque
-                : 0;
+        void Update()
+        {
+            syncWheel(FL, FLObject);
+            syncWheel(FR, FRObject);
+            syncWheel(RL, RLObject);
+            syncWheel(RR, RRObject);
+        }
 
-        FL.brakeTorque = brake;
-        FR.brakeTorque = brake;
-        RL.brakeTorque = brake;
-        RR.brakeTorque = brake;
-    }
+        void syncWheel(
+            WheelCollider wheel,
+            GameObject obj
+        )
+        {
+            wheel.GetWorldPose(
+                out var position,
+                out var rotation
+            );
 
-    void Update() {
-        syncWheel(FL, FLObject);
-        syncWheel(FR, FRObject);
-        syncWheel(RL, RLObject);
-        syncWheel(RR, RRObject);
-    }
-
-    void syncWheel(
-        WheelCollider wheel,
-        GameObject obj
-    ) {
-        wheel.GetWorldPose(
-            out var position,
-            out var rotation
-        );
-
-        obj.transform.position = position;
-        // 修正 Cylinder 自身轴向
-        obj.transform.rotation =
-            rotation * Quaternion.Euler(0, 0, 90);
+            obj.transform.position = position;
+            // 修正 Cylinder 自身轴向
+            obj.transform.rotation =
+                rotation * Quaternion.Euler(0, 0, 90);
+        }
     }
 }
